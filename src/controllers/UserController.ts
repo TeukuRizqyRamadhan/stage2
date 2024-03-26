a708583784bdaddd6835b0fa3dd929ab38bfc207
TeukuRizqyRamadhan <kikidotid@gmail.com>
2024-03-26 12:08:35 +0700

    prisma

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,23 @@
+import UserService from "../services/UserService";
+import { Request, Response } from "express";
+
+export default new class UserController {
+    find(req: Request, res: Response) {
+        UserService.find(req, res)
+    }
+    findById(req: Request, res: Response) {
+        UserService.findById(req, res)
+    }
+
+    updateUser(req: Request, res: Response) {
+        UserService.updateUser(req, res)
+    }
+
+    deleteUser(req: Request, res: Response) {
+        UserService.deleteUser(req, res)
+    }
+
+    addUser(req: Request, res: Response) {
+        UserService.addUser(req, res)
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import router from './routers/router'
+
+const app = express()
+app.use(express.json())
+dotenv.config()
+
+app.use("/api/v1/", router)
+
+app.listen(process.env.PORT, () => {
+    console.log(`sukses berjalan di port: ${process.env.PORT}`)
+})
